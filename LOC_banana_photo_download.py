ac90b2790fca910bb55e0f1dc6aef48306d5bab9
Genevieve Milliken <gmillike@pratt.edu>
2020-01-17 11:49:34 -0500

    Update LOC_banana_photo_download.py

--- LOC_banana_photo_download.py
+++ LOC_banana_photo_download.py
@@ -1,17 +1,19 @@
 import json
-import requests
+import urllib.request
 
 def download_image(url, file_path, caption):
 	full_path = file_path + caption + '.jpg'
 	try:
 		urllib.request.urlretrieve(url, full_path)
-	except FileNotFoundError:
+	except TypeError:
 		pass
 
-# open json file, extract information, write images to file
-with open('LOC_Bananas_PortNOLA.json') as f_object:
-	text = json.load(f_object)
-	for data_dict in text:
-		caption = data_dict['LOC_Call_Number']
-		url = data_dict['image_URL']
-		download_image(url, "Images/", caption)
+# # open json file, extract information, write images to file
+# with open('LOC_Bananas_Port_NOLA.json') as f_object:
+# 	text = json.load(f_object)
+# 	for data_dict in text:
+# 		caption = data_dict['LOC_Call_Number']
+# 		url = data_dict['image_URL']
+# 		download_image(url, "Images/", caption)
+
+# download_image('http://cdn.loc.gov/service/pnp/det/4a10000/4a17000/4a17000/4a17030v.jpg', "Images/", "test")
